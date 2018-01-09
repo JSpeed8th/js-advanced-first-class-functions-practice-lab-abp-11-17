427e401ddb8b529fbfd44714c259a95cea855b2e
JSpeed8th <jspeed8th@gmail.com>
2018-01-09 02:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ return newArray;
 };
 
 function driversByName(drivers) {
-  const newArray = drivers.name.sort()
+  const newArray = [...drivers['name']].sort(function (name1, name2) {
+    
+  })
   //returns a new array of `driver` objects sorted by their `name` attribute from A to Z
   return newArray;
 };
