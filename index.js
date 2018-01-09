fecef19ab2f524aa52b4e942023c867c228b2fa0
JSpeed8th <jspeed8th@gmail.com>
2018-01-09 03:02:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,12 +37,19 @@ function driversByName(drivers) {
   return newArray;
 };
 
+/*const drivers = [
+  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+];*/
 function totalRevenue(drivers) {
-  let counter = 0;
-  drivers.forEach(function (el, i, arr){
-    return el.revenue += counter;
+  let totalRevenue = 0;
+  drivers.forEach(function (driver){
+    totalRevenue += driver.revenue;
     //returns sum of Revenue.
-  })
+  });
 
-  return counter;
+  return totalRevenue;
 };
