4c2bfd2e01e7b83db78adb63ea7350f095fcb574
JSpeed8th <jspeed8th@gmail.com>
2018-01-09 02:29:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function logDriverNames (drivers) {
+
+drivers.forEach(function (el, i, arr) {
+
+    console.log(el.name);
+  })
+};
+
+function logDriversByHometown (drivers, location) {
+
+  drivers.forEach(function (el, i, arr) {
+    if (el.hometown === location) {
+      console.log(el.name);
+    }
+  })
+};
+
+function driversByRevenue(drivers) {
+  drivers.sort(function (rev1, rev2) {
+    let returnUni = rev1.revenue - rev2.revenue;
+    if (returnUni < 0) {
+
+      return -1;
+    } if (returnUni > 0){
+      return 1;
+    }
+})
+const driversBasedOnRev = [...drivers]
+return driversBasedOnRev;
+};
